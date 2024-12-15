9e1028d61ae89d336864e75791fba80dcf53e90a
rafael.amorim <raffaekk@gmail.com>
2024-12-15 12:11:25 -0300

    feat:Models da camada mart

---
+++ dbt_suprimentos/models/mart/suprimentos/fornecedor.sql
@@ -0,0 +1,25 @@
+
+
+{{
+    config( materialized = 'incremental' )
+
+}}
+
+
+
+WITH source_int_fornecedor
+    AS (
+        SELECT
+            *
+        FROM {{ ref( 'int_fornecedor' ) }} sis
+            {% if is_incremental() %}
+                WHERE sis."CD_FORNECEDOR" > (SELECT MAX("CD_FORNECEDOR") FROM {{ this }}) -- Carregar apenas novos dados
+            {% endif %}
+),
+mrt_fornecedor
+    AS (
+        SELECT
+            *
+        FROM source_int_fornecedor
+)
+SELECT * FROM mrt_fornecedor
