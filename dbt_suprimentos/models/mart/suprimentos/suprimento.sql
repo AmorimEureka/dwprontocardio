50a6e353a7df621a4f69272f13fa8bdb8f7647f9
rafael.amorim <raffaekk@gmail.com>
2024-12-30 14:44:09 -0300

    feat:Inclusão do modelo suprimento

---
+++ dbt_suprimentos/models/mart/suprimentos/suprimento.sql
@@ -0,0 +1,24 @@
+
+
+
+
+{{
+    config( materialized = 'table' )
+
+}}
+
+
+
+WITH source_int_suprimento
+    AS (
+        SELECT
+            *
+        FROM {{ ref( 'int_suprimento' ) }}
+),
+mrt_suprimento
+    AS (
+        SELECT DISTINCT
+            *
+        FROM source_int_suprimento
+)
+SELECT * FROM mrt_suprimento
