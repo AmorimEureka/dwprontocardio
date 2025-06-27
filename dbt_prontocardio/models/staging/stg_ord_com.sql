d5f75469cfd8cd4af6a124972f6210ff430f6163
prontocardio <prontocardio@prontocardio.com>
2025-06-27 13:08:12 +0000

    fix: Alteracoes em stg_ord_com e stg_sol_com: 	- Especificacao da estrategia incremental: 		- incremental_strategy = 'merge'
    	- Especificacao dos campos p/ realizacao do merge update
    
    	- Alteracao na condicao incremental:
    		- Incrementacao mista:
    			- Ultimos 30 dias p/ atualizar os campos do merge update
    			- Codigos de solicitacoes maior que o atual p/
    			novos registros
    
    	- Filtro p/ remover linhas duplicadas da camada raw:
    		- Classificacao de Particionamento p/ remover registros duplicados

--- dbt_prontocardio/models/staging/stg_ord_com.sql
+++ dbt_prontocardio/models/staging/stg_ord_com.sql
@@ -1,6 +1,8 @@
 {{
     config( materialized = 'incremental',
-            unique_key = '"CD_ORD_COM"' )
+            incremental_strategy = 'merge',
+            unique_key = '"CD_ORD_COM"',
+            merge_update_columns = ['"TP_SITUACAO"', '"CD_MOT_CANCEL"', '"DT_CANCELAMENTO"'] )
 }}
 
 WITH source_ord_com
@@ -24,28 +26,37 @@ WITH source_ord_com
             , sis."DT_EXTRACAO"
         FROM {{ source('raw_mv' , 'ord_com') }} sis
         {% if is_incremental() %}
-        WHERE sis."CD_ORD_COM"::BIGINT > ( SELECT MAX("CD_ORD_COM") FROM {{this}} )
+        WHERE (
+                sis."DT_ORD_COM" >= (current_date - interval '30 day')
+                or
+                sis."CD_ORD_COM"::BIGINT > ( SELECT MAX("CD_ORD_COM") FROM {{this}} )
+                )
         {% endif %}
 ),
 treats
     AS (
         SELECT
-            "CD_ORD_COM"::BIGINT
-            , "CD_ESTOQUE"::BIGINT
-            , "CD_FORNECEDOR"::BIGINT
-            , "CD_SOL_COM"::BIGINT
-            , "CD_MOT_CANCEL"::BIGINT
-            , "CD_USUARIO_CRIADOR_OC"::VARCHAR(30)
-            , "CD_ULTIMO_USU_ALT_OC"::VARCHAR(30)
-            , "DT_ORD_COM"::DATE
-            , "DT_PREV_ENTREGA"::DATE
-            , "DT_CANCELAMENTO"::TIMESTAMP
-            , "DT_AUTORIZACAO"::TIMESTAMP
-            , "DT_ULTIMA_ALTERACAO_OC"::DATE
-            , "TP_SITUACAO"::VARCHAR(1)
-            , "TP_ORD_COM"::VARCHAR(1)
-            , "SN_AUTORIZADO"::VARCHAR(1)
-            , "DT_EXTRACAO"::TIMESTAMP
-        FROM source_ord_com
-)
-SELECT * FROM treats
+            *,
+            ROW_NUMBER() OVER (PARTITION BY "CD_ORD_COM" ORDER BY "DT_EXTRACAO" DESC) as rn
+        FROM (
+            SELECT
+                "CD_ORD_COM"::BIGINT
+                , "CD_ESTOQUE"::BIGINT
+                , "CD_FORNECEDOR"::BIGINT
+                , "CD_SOL_COM"::BIGINT
+                , "CD_MOT_CANCEL"::BIGINT
+                , "CD_USUARIO_CRIADOR_OC"::VARCHAR(30)
+                , "CD_ULTIMO_USU_ALT_OC"::VARCHAR(30)
+                , "DT_ORD_COM"::DATE
+                , "DT_PREV_ENTREGA"::DATE
+                , "DT_CANCELAMENTO"::TIMESTAMP
+                , "DT_AUTORIZACAO"::TIMESTAMP
+                , "DT_ULTIMA_ALTERACAO_OC"::DATE
+                , "TP_SITUACAO"::VARCHAR(1)
+                , "TP_ORD_COM"::VARCHAR(1)
+                , "SN_AUTORIZADO"::VARCHAR(1)
+                , "DT_EXTRACAO"::TIMESTAMP
+            FROM source_ord_com
+        ) base
+    )
+SELECT * FROM treats WHERE rn = 1
